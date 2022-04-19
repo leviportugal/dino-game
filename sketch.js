69731bc564bfe008662bf4641db2f44b589ba2cc
leviportugal <levi.ap.2007@gmail.com>
2022-04-19 18:01:27 -0300

    aula 12

--- sketch.js
+++ sketch.js
@@ -2,10 +2,11 @@ var trex, trex_running, edges;
 var groundImage;
 var chao;
 var chao2;
+var nuvem_mg
 function preload(){
   trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
   groundImage = loadImage("ground2.png")
-
+nuvem_mg = loadImage ("cloud.png")
 
 }
 
@@ -49,6 +50,37 @@ chao.x = chao.width/2;
  //impedir que o trex caia
   trex.collide(chao2 )  
   drawSprites();
+geranuvem ()
+geracacto()
+}
+
+function geranuvem (){
+if (frameCount%50===0){
+
+  var nuvem 
+  nuvem=createSprite (600,50,40,10)
+  nuvem.velocityX=  -4
+  nuvem.addImage (nuvem_mg)
+  nuvem.y = Math.round(random(10,100))
+}
+}
+
+function geracacto (){
+if (frameCount%80===0){
+
+  var cacto
+  cacto=createSprite (500, 170, 20,40)
+  cacto.velocityX = -2
 
 
 }
+
+
+
+
+
+
+}
+
+
+
