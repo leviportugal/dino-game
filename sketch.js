68442a629bf8c3f9c17e215f1430bfa2a8776a4d
leviportugal <levi.ap.2007@gmail.com>
2022-04-21 19:01:37 -0300

    aula13

--- sketch.js
+++ sketch.js
@@ -3,10 +3,22 @@ var groundImage;
 var chao;
 var chao2;
 var nuvem_mg
+var c1,c2,c3,c4,c5,c6
+var score = 0 
+
+
+
+
 function preload(){
   trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
   groundImage = loadImage("ground2.png")
 nuvem_mg = loadImage ("cloud.png")
+c1 = loadImage ("obstacle1.png")
+c2 = loadImage ("obstacle2.png")
+c3 = loadImage ("obstacle3.png")
+c4 = loadImage ("obstacle4.png")
+c5 = loadImage ("obstacle5.png")
+c6 = loadImage ("obstacle6.png")
 
 }
 
@@ -35,17 +47,17 @@ function draw(){
   //registrando a posição y do trex
  if (chao.x<0) {
 chao.x = chao.width/2;
-
-
  }
+ score++
+ textSize (20)
+ text (score,550,30 )
  
-  
   //pular quando tecla de espaço for pressionada
-  if(keyDown("space") && trex.collide (chao)){
-    trex.velocityY = -10; 
+  if(keyDown("space") && trex.y >= 160    ){
+    trex.velocityY = -13; 
 }  
   
-  trex.velocityY = trex.velocityY + 0.5;
+  trex.velocityY = trex.velocityY + 0.9;
   
  //impedir que o trex caia
   trex.collide(chao2 )  
@@ -62,6 +74,7 @@ if (frameCount%50===0){
   nuvem.velocityX=  -4
   nuvem.addImage (nuvem_mg)
   nuvem.y = Math.round(random(10,100))
+nuvem.lifetime=160
 }
 }
 
@@ -69,17 +82,31 @@ function geracacto (){
 if (frameCount%80===0){
 
   var cacto
-  cacto=createSprite (500, 170, 20,40)
-  cacto.velocityX = -2
-
+  cacto=createSprite (600, 170, 20,40)
+  cacto.velocityX = -5
+  cacto.scale = 0.5
+  cacto.lifetime = 315
+var hand= Math.round (random(1,6))
+switch (hand){
+case 1: cacto.addImage (c1)
+break
+case 2: cacto.addImage (c2)
+break
+case 3: cacto.addImage (c3)
+break
+case 4: cacto.addImage (c4)
+break
+case 5: cacto.addImage (c5)
+break
+case 6: cacto.addImage (c6)
+break
 
 }
 
 
 
 
-
-
+}
 }
 
 
